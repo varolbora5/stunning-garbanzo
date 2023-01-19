bfa3d6ce3f2f83f1f96d1fd54e22f31a5d27d7ae
Bora <varolbora5@gmail.com>
2023-01-19 18:06:49 +0100

    regenerate button works

--- terrain.py
+++ terrain.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 import numpy as np
 from noise import snoise2
-
+import random
 
 class Terrain:
 	def __init__(self, width = 100, height = 100, scale = 40, octaves = 8, persistence = 0.6, lacunarity = 2.0):
@@ -12,6 +13,14 @@ class Terrain:
 		self.persistence = persistence
 		self.lacunarity = lacunarity
 		self.terrain = np.zeros((width, height))
+		self.fig, self.ax = plt.subplots()
+		self.fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+	def next(self, event):
+        self.terrain = np.zeros((self.width, self.height))
+        self.generate()
+		self.l.set_data(self.terrain)
+		plt.draw()
 
 	def generate(self):
 		# For each row in the terrain array
@@ -20,22 +29,25 @@ class Terrain:
 			for y in range(self.height):
 				# Get the noise value for that position
 				self.terrain[x][y] = snoise2(x / self.scale,
-																y / self.scale,
-																octaves= self.octaves,
-																persistence=self.persistence,
-																lacunarity=self.lacunarity,
-																repeatx=self.width,
-																repeaty=self.height,
-																base=0)
+                                 y / self.scale,
+                                 octaves= self.octaves,
+                                 persistence=self.persistence,
+                                 lacunarity=self.lacunarity,
+                                 repeatx=self.width,
+                                 repeaty=self.height,
+                                 base=random.randint(1, 10))
 
 	# Normalize the data so that it is between 0 and 1
 		self.terrain = (self.terrain + 1) / 2
 
 	# Round the data to 0 or 1
-		self.terrain = np.where(self.terrain > 0.5, 1, 0)
+		# self.terrain = np.where(self.terrain > 0.5, 1, 0)
 		return self
 
 	def show(self):
-		plt.imshow(self.terrain, cmap='Greens')
+        self.l = self.ax.imshow(self.terrain, cmap='Greens')
+		axbutton = plt.axes([0.8, 0.05, 0.1, 0.075])
+		button = Button(axbutton, 'Re-plot')
+		button.on_clicked(self.next)
 		plt.show()
 		return self
